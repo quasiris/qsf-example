5be892ace8511fcc10bf555c77bd842c0b104ac6
Thomas Bärwinkel <tbl@gmx.de>
2019-05-18 23:56:11 +0200

    refactor package

--- src/main/java/com/quasiris/qsf/QsfExampleApplication.java
+++ src/main/java/com/quasiris/qsf/QsfExampleApplication.java
@@ -1,4 +1,4 @@
-package com.quasiris.qsfexample;
+package com.quasiris.qsf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/quasiris/qsf/QsfExampleApplicationTests.java
+++ src/test/java/com/quasiris/qsf/QsfExampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.quasiris.qsfexample;
+package com.quasiris.qsf;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
