ce19833ec99c48a2b5a83b050139c336497ddc7e
Thomas Bärwinkel <tbl@gmx.de>
2019-05-18 22:39:21 +0200

    initial import

---
+++ src/main/java/com/quasiris/qsfexample/QsfExampleApplication.java
@@ -0,0 +1,13 @@
+package com.quasiris.qsfexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QsfExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QsfExampleApplication.class, args);
+	}
+
+}
